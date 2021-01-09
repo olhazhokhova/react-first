8ecf72013cc274b0ff1c78953a171ff0d5eec2e9
olhazhokhova <zhokhova.olha@gmail.com>
2021-01-09 17:38:41 +0200

    add ref to textarea

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,18 +4,25 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
   let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
-    return <div className={s.postsWrap}>
-      <h3>My posts</h3>
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
+  return <div className={s.postsWrap}>
+    <h3>My posts</h3>
+    <div>
       <div>
-        <div>
-          <textarea name="" id="" cols="50" rows="5"></textarea>
-        </div>
-        <button>Add post</button>
+        <textarea ref={ newPostElement } name="" id="" cols="50" rows="5"></textarea>
       </div>
-      <div className={s.posts}>
-        { postsElements }
-      </div>
-    </div>;
+      <button onClick={ addPost }>Add post</button>
+    </div>
+    <div className={s.posts}>
+      { postsElements }
+    </div>
+  </div>;
 }
 
 export default MyPosts;
